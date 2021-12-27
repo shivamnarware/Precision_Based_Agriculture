9220e952be891ba3b63d7261758f6493b12c2472
shivamnarware <shivamnarware97@gmail.com>
2021-12-27 20:07:01 +0530

    "added_route"

--- backend/routes/private.js
+++ backend/routes/private.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const { getPrivateData, postComments } = require("../controllers/private")
+const { getPrivateData, postComments, getComments } = require("../controllers/private")
 const {protected}=require('../middleware/auth')
 
 router.route("/").get(protected,getPrivateData);
+
+router.route("/comments").get(protected,getComments)
 
 router.route("/comments").post(protected,postComments);
 

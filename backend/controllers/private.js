1ef5eb5671ee8c613e8824623c280271da248577
shivamnarware <shivamnarware97@gmail.com>
2021-12-29 22:19:32 +0530

    "added_comment"

--- backend/controllers/private.js
+++ backend/controllers/private.js
@@ -1,31 +1,30 @@
 // const User=require("../models/Users");
-const Comment=require("../models/Comments");
+const Comment = require("../models/Comments");
 
 exports.getPrivateData = (req, res, next) => {
     res.status(200).json({
-       sucess:"true",
-       data:"you got access to private route"
+        sucess: "true",
+        data: "you got access to private route"
     });
 };
 
-exports.getComments=async(req,res,next)=>{
-    const data= await Comment.find({}).populate("reply");
-    console.log(data);
+exports.getComments = async (req, res, next) => {
+    const data = await Comment.find({path: 'reply',populate: { path: 'reply' }});
     res.status(200).json({
-        sucess:"true",
-        data:data
-     });  
+        sucess: "true",
+        data: data
+    });
 }
 
-exports.postComments=async(req,res,next)=>{
-    const comment=req.body;
-    const data=await Comments.create({text:comment});
+exports.postComments = async (req, res, next) => {
+    const comment = req.body;
+    const data = await Comment.create({ text: comment.data });
     data.author._id = req.user._id;
     data.author.username = req.user.username;
     console.log(data);
     data.save();
     res.status(200).json({
-        sucess:"true",
-        data:data
+        sucess: "true",
+        data: data
     });
 }
